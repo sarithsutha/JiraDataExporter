bd3d5d2bdb76da56ac364926ef625523164b9eee
Sarith Sutha Makam <sarithsutha.makam@stonegatemtg.com>
2018-06-14 15:03:10 -0400

    Blank solution and projects

---
+++ JiraDataExporter.CLI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JiraDataExporter.CLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
